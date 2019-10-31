0173c54884ae3af937774d7ad9682656212d8ec9
gdstout <gdstout27@gmail.com>
2019-10-30 22:49:30 -0500

    test

--- src/instruction_impl.c
+++ src/instruction_impl.c
@@ -17,3 +17,18 @@ void ANDI(uint64_t *X, uint8_t dest, uint8_t source, uint64_t immediate){
     X[dest] = X[source] & immediate;
 }
 
+// missing methods here
+
+void EOR(uint64_t *X, uint8_t dest, uint8_t source1, uint8_t source2){
+    X[dest] = X[source1] ^ X[source2];
+}
+
+void EORI(uint64_t *X, uint8_t dest, uint8_t source, uint64_t immediate){
+    X[dest] = X[source] ^ X[immediate];
+}
+
+void LDUR(uint64_t *X, uint64_t *stack, uint8_t dest, uint8_t address, uint16_t offset){
+       
+}
+
+
